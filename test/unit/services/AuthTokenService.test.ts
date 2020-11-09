b5beb5d9f7de93256a62005625306afa33f6419f
Fabio Oshiro <fabio.oshiro@calindra.com.br>
2020-11-09 10:18:18 -0300

    AuthTokenService test

---
+++ test/unit/services/AuthTokenService.test.ts
@@ -0,0 +1,26 @@
+import { AuthTokenService } from "../../../src/modules/authToken/services/AuthTokenService"
+
+describe("AuthTokenService", () => {
+
+    let authTokenService: AuthTokenService
+    let conf = {
+        CLIENT_ID: 'ame-previsul',
+        CLIENT_SECRET: process.env.CLIENT_SECRET,
+        CLIENT_SCOPE: 'hub.previsul',
+        URL_AUTHORIZATION: 'https://gateway.b2sky.io/hub-previsul/oauth/token'
+    }
+
+    beforeEach(() => {
+        let parameterStore: any = {
+            getSecretValue: (confName) => {
+                return conf[confName]
+            }
+        }
+        authTokenService = new AuthTokenService(parameterStore)
+    })
+
+    it("should do the auth", async () => {
+        let res = await authTokenService.retrieveAuthorization()
+        console.log(res)
+    })
+})
