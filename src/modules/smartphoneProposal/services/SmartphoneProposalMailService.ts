4771078537f6f492f3abafea9b23ee53ee62d751
Marcelo Madeira <marcelo.madeira@calindra.com.br>
2021-03-03 09:32:35 -0300

    Melhorando o código de envio

--- src/modules/smartphoneProposal/services/SmartphoneProposalMailService.ts
+++ src/modules/smartphoneProposal/services/SmartphoneProposalMailService.ts
@@ -36,60 +36,68 @@ export class SmartphoneProposalMailService {
         const email: string = unsignedPayment?.attributes?.customPayload?.clientEmail
         const dataToSendMail = await this.formatMailJsonParseInfo(unsignedPayment)
         let emailTemplate = path.resolve(__dirname, '../../../../mail_template/smartphone_mail.html')
-        try {
-            let template = await readFile(emailTemplate, 'utf-8')
-            let body = template
-            // DADOS DO USUARIO
-                .replace(/@@secury_user_name@@/g, `${dataToSendMail.securityName}`)
-                .replace(/@@secury_user_cpf@@/g, `${dataToSendMail.securityUserCpf}`)
-                .replace(/@@secury_user_address@@/g, `${dataToSendMail.securityAddress}`)
-                .replace(/@@secury_user_number@@/g, `${dataToSendMail.securityAddressNumber}`)
-                .replace(/@@secury_user_district@@/g, `${dataToSendMail.securityAddressDistrict}`)
-                .replace(/@@secury_user_city@@/g, `${dataToSendMail.securityAddressCity}`)
-                .replace(/@@secury_user_cep@@/g, `${dataToSendMail.securityDataUserCep}`)
-                .replace(/@@secury_user_uf@@/g, `${dataToSendMail.securityAddressUf}`)
-                // DADOS DO REPRESENTANTE DO SEGURO
-                .replace(/@@social_reazon@@/g, `${dataToSendMail.SecurityRepresentationSocialReazon}`)
-                .replace(/@@cnpj@@/g, `${dataToSendMail.SecurityRepresentationCnpj}`)
-                .replace(/@@cod_susep@@/g, `${dataToSendMail.SecurityRepresentationCodSusep}`)
-                // DADOS DA SEGURADORA
-                .replace(/@@secury_data_social_reazon@@/g, `${dataToSendMail.securityDataSocialReazon}`)
-                .replace(/@@secury_data_cpf@@/g, `${dataToSendMail.securityDataCpf}`)
-                // DADOS DO CORRETOR
-                .replace(/@@broker_name@@/g, `${dataToSendMail.brokerName}`)
-                .replace(/@@broker_susep@@/g, `${dataToSendMail.brokerCodSusep}`)
-                // DADOS DO SEGURO
-                .replace(/@@secury_data_branch@@/g, `${dataToSendMail.securyDataBranch}`)
-                .replace(/@@secury_data_individual_ticket@@/g, `${dataToSendMail.securyDataIndividualTicket}`)
-                .replace(/@@secury_data_emission_date@@/g, `${dataToSendMail.securyDataEmissionDate}`)
-                .replace(/@@initial_surance_term@@/g, `${dataToSendMail.securyDataInitialSuranceTerm}`)
-                .replace(/@@final_surance_term@@/g, `${dataToSendMail.securyDataFinalSuranceTerm}`)
-                // COBERTURAS CONTRATADAS
-                .replace(/@@max_limit_thieft@@/g, `${dataToSendMail.maxLimitThieft}`)
-                .replace(/@@pos_thieft@@/g, `${dataToSendMail.posThieft}`)
-                .replace(/@@prize_thieft@@/g, `${dataToSendMail.prizeThieft}`)
-                .replace(/@@lack_thieft@@/g, `${dataToSendMail.lackThieft}`)
-                .replace(/@@max_limit_acidental@@/g, `${dataToSendMail.maxLimitAcidental}`)
-                .replace(/@@pos_acidental@@/g, `${dataToSendMail.posAcidental}`)
-                .replace(/@@prize_acidental@@/g, `${dataToSendMail.prizeAcidental}`)
-                .replace(/@@lack_acidental@@/g, `${dataToSendMail.lackAcidental}`)
-                .replace(/@@product_description@@/g, `${dataToSendMail.productDescription}`)
-                .replace(/@@model@@/g, `${dataToSendMail.model}`)
-                .replace(/@@mark@@/g, `${dataToSendMail.mark}`)
-                .replace(/@@payment_form@@/g, `${dataToSendMail.paymentForm}`)
-                .replace(/@@liquid_prize@@/g, `${dataToSendMail.liquidPrice}`)
-                .replace(/@@iof@@/g, `${dataToSendMail.iof}`)
-                .replace(/@@total_prize@@/g, `${dataToSendMail.totalPrize}`)
 
-            const forceEmailSender = await this.parameterStore.getSecretValue("FORCE_EMAIL_SENDER")
-            const emailFrom = forceEmailSender ? forceEmailSender : 'no-reply@amedigital.com'
+        let template = await readFile(emailTemplate, 'utf-8')
+        let body = template
+        // DADOS DO USUARIO
+            .replace(/@@secury_user_name@@/g, `${dataToSendMail.securityName}`)
+            .replace(/@@secury_user_cpf@@/g, `${dataToSendMail.securityUserCpf}`)
+            .replace(/@@secury_user_address@@/g, `${dataToSendMail.securityAddress}`)
+            .replace(/@@secury_user_number@@/g, `${dataToSendMail.securityAddressNumber}`)
+            .replace(/@@secury_user_district@@/g, `${dataToSendMail.securityAddressDistrict}`)
+            .replace(/@@secury_user_city@@/g, `${dataToSendMail.securityAddressCity}`)
+            .replace(/@@secury_user_cep@@/g, `${dataToSendMail.securityDataUserCep}`)
+            .replace(/@@secury_user_uf@@/g, `${dataToSendMail.securityAddressUf}`)
+            // DADOS DO REPRESENTANTE DO SEGURO
+            .replace(/@@social_reazon@@/g, `${dataToSendMail.SecurityRepresentationSocialReazon}`)
+            .replace(/@@cnpj@@/g, `${dataToSendMail.SecurityRepresentationCnpj}`)
+            .replace(/@@cod_susep@@/g, `${dataToSendMail.SecurityRepresentationCodSusep}`)
+            // DADOS DA SEGURADORA
+            .replace(/@@secury_data_social_reazon@@/g, `${dataToSendMail.securityDataSocialReazon}`)
+            .replace(/@@secury_data_cpf@@/g, `${dataToSendMail.securityDataCpf}`)
+            // DADOS DO CORRETOR
+            .replace(/@@broker_name@@/g, `${dataToSendMail.brokerName}`)
+            .replace(/@@broker_susep@@/g, `${dataToSendMail.brokerCodSusep}`)
+            // DADOS DO SEGURO
+            .replace(/@@secury_data_branch@@/g, `${dataToSendMail.securyDataBranch}`)
+            .replace(/@@secury_data_individual_ticket@@/g, `${dataToSendMail.securyDataIndividualTicket}`)
+            .replace(/@@secury_data_emission_date@@/g, `${dataToSendMail.securyDataEmissionDate}`)
+            .replace(/@@initial_surance_term@@/g, `${dataToSendMail.securyDataInitialSuranceTerm}`)
+            .replace(/@@final_surance_term@@/g, `${dataToSendMail.securyDataFinalSuranceTerm}`)
+            // COBERTURAS CONTRATADAS
+            .replace(/@@max_limit_thieft@@/g, `${dataToSendMail.maxLimitThieft}`)
+            .replace(/@@pos_thieft@@/g, `${dataToSendMail.posThieft}`)
+            .replace(/@@prize_thieft@@/g, `${dataToSendMail.prizeThieft}`)
+            .replace(/@@lack_thieft@@/g, `${dataToSendMail.lackThieft}`)
+            .replace(/@@max_limit_acidental@@/g, `${dataToSendMail.maxLimitAcidental}`)
+            .replace(/@@pos_acidental@@/g, `${dataToSendMail.posAcidental}`)
+            .replace(/@@prize_acidental@@/g, `${dataToSendMail.prizeAcidental}`)
+            .replace(/@@lack_acidental@@/g, `${dataToSendMail.lackAcidental}`)
+            .replace(/@@product_description@@/g, `${dataToSendMail.productDescription}`)
+            .replace(/@@model@@/g, `${dataToSendMail.model}`)
+            .replace(/@@mark@@/g, `${dataToSendMail.mark}`)
+            .replace(/@@payment_form@@/g, `${dataToSendMail.paymentForm}`)
+            .replace(/@@liquid_prize@@/g, `${dataToSendMail.liquidPrice}`)
+            .replace(/@@iof@@/g, `${dataToSendMail.iof}`)
+            .replace(/@@total_prize@@/g, `${dataToSendMail.totalPrize}`)
+
+        const forceEmailSender = await this.parameterStore.getSecretValue("FORCE_EMAIL_SENDER")
+        const accessKeyId = await this.parameterStore.getSecretValue("MAIL_ACCESS_KEY_ID")
+        const secretAccessKey = await this.parameterStore.getSecretValue("MAIL_SECRET_ACCESS_KEY")
+        const emailFrom = forceEmailSender ? forceEmailSender : 'no-reply@amedigital.com'
+        if (accessKeyId && secretAccessKey) {
             log.debug(`EmailFrom:${emailFrom}`)
-            const sendResult = await EmailSender.sendEmail(emailFrom, email, body)
-            return sendResult.MessageId
-        } catch (e) {
-            console.debug('ERRO ENVIO', e);
-            throw 'Erro ao enviar email de cupom'
+            try {
+                const sendResult = await EmailSender.sendEmail(emailFrom, email, body, accessKeyId, secretAccessKey)
+                return sendResult.MessageId
+            } catch (e) {
+                console.error('Email not sent, error', e);
+                throw 'Error during sending email'
+            }
+        } else {
+            throw "AWS Mail credentials not configured, MAIL_ACCESS_KEY_ID & MAIL_SECRET_ACCESS_KEY"
         }
+
     }
 
     async formatMailJsonParseInfo(MailInfo) {
