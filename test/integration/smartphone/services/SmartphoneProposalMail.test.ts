186d131750c3d041565d9d57cf4b2875b5904b88
reynaldo1991 <reynaldo.souza@calindra.com.br>
2021-03-04 14:49:17 -0300

    Ajuste do teste do SmarphoneProposal

--- test/integration/smartphone/services/SmartphoneProposalMail.test.ts
+++ test/integration/smartphone/services/SmartphoneProposalMail.test.ts
@@ -1,4 +1,5 @@
 import {initDependencies, iocContainer} from "../../../../src/inversify/inversify.config";
+import { SmartphoneProposalRepository } from "../../../../src/modules/smartphoneProposal/repository/SmartphoneProposalRepository";
 import { SmartphoneProposalMailService } from "../../../../src/modules/smartphoneProposal/services/SmartphoneProposalMailService";
 
 initDependencies()
@@ -7,11 +8,14 @@ jest.setTimeout(30000)
 
 describe("SmartphoneProposalMail", () => {
     let smartphoneProposalMailService: SmartphoneProposalMailService
+    let smartphoneProposalRepository: SmartphoneProposalRepository
     beforeAll(async () => {
         smartphoneProposalMailService = iocContainer.get("SmartphoneProposalMailService")
+        smartphoneProposalRepository = iocContainer.get("SmartphoneProposalRepository")
     }) 
     it("Envia o e-mail para o segurado", async () => {
-        const pass = '49917e28-8d64-47f7-a280-93c28481eab9';
+        let getPass = await smartphoneProposalRepository.listProposal()
+        let pass = getPass[0].id
         const emailId = await smartphoneProposalMailService.sendSellingEmail(pass) 
         expect(emailId).toBeDefined()
     })
