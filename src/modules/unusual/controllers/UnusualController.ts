681df55ca7b2d821e2148bbd1ab4581688794a52
reynaldo1991 <reynaldo.souza@calindra.com.br>
2021-03-15 16:38:54 -0300

    Reprocesando a SoldProposal.

--- src/modules/unusual/controllers/UnusualController.ts
+++ src/modules/unusual/controllers/UnusualController.ts
@@ -1,6 +1,8 @@
 import {inject, injectable} from "inversify"
-import {Path, Route, SuccessResponse, Response, Post } from "tsoa"
+import {Path, Route, SuccessResponse, Response, Post, Get } from "tsoa"
+import { ApiError } from "../../../errors/ApiError"
 import {getLogger} from "../../../server/Logger"
+import { SmartphoneProposalService } from "../../smartphoneProposal/services/SmartphoneProposalService"
 import { UnusualService } from "../services/UnusualService"
 
 const logger = getLogger("UnusualController")
@@ -12,18 +14,33 @@ export class UnusualController {
     constructor(        
         @inject("UnusualService") 
         private unusualService: UnusualService,
+        @inject("SmartphoneProposalService") 
+        private smartphoneProposalService: SmartphoneProposalService,
     ) {
     }
     
     @Response(404, 'NotFound')
     @SuccessResponse("200", "Retrieved")
-    @Post("/proposal_email/{pass}/{email}/sendEmail")
+    @Get("/proposal_email/{pass}/{email}/sendEmail")
     public async sendMailProposalWithParams(@Path() pass: string, email: string) {
         try {
             logger.info('E-mail com o id de compra:', pass)
             await this.unusualService.sendSellingEmailWithParams(pass, email)
         } catch (e) {
             logger.error(e.message)
+        }
+    }
+
+    @Response(404, 'NotFound')
+    @SuccessResponse("200", "Retrieved")
+    @Get("/update_old_clients/{proposalId}")
+    public async updateProposal(@Path() proposalId: string) {
+        logger.info('Proposal Id %j', proposalId);
+        try {
+            return await this.smartphoneProposalService.updateOldCustumersProposal(proposalId)
+        } catch (e) {
+            logger.error(e.message)
+            throw new ApiError("Update not sent", 500, `Update not sent`)
         }
     }
 }
