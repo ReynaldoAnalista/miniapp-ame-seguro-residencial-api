d2d7e6da4d9c253c0f3a48ad6c05454f5dc24fad
Izabella Assad <izabellaassad@gmail.com>
2021-04-05 16:44:03 -0300

    headers do request pet

--- src/modules/authToken/services/RequestService.ts
+++ src/modules/authToken/services/RequestService.ts
@@ -1,15 +1,18 @@
 import axios from "axios";
-import {inject, injectable} from "inversify";
-import {TYPES} from "../../../inversify/inversify.types";
-import {ParameterStore} from "../../../configs/ParameterStore";
-import {AuthTokenService} from "./AuthTokenService";
-import {getLogger} from "../../../server/Logger";
-import {Tenants} from "../../default/model/Tenants";
-import curlirize from 'axios-curlirize'
+import { inject, injectable } from "inversify";
+import { TYPES } from "../../../inversify/inversify.types";
+import { ParameterStore } from "../../../configs/ParameterStore";
+import { AuthTokenService } from "./AuthTokenService";
+import { getLogger } from "../../../server/Logger";
+import { Tenants } from "../../default/model/Tenants";
+import curlirize from "axios-curlirize";
 
 const log = getLogger("RequestService");
 
-if(process.env.NODE_ENV === 'homolog' || process.env.NODE_ENV === 'development'){
+if (
+    process.env.NODE_ENV === "homolog" ||
+    process.env.NODE_ENV === "development"
+) {
     curlirize(axios);
 }
 
@@ -21,63 +24,74 @@ enum Methods {
 }
 
 enum Endpoints {
-    RESIDENTIAL_URL_PLANS = 'URL_PLANS',
-    RESIDENTIAL_URL_ZIPCODE = 'URL_ZIPCODE',
-    RESIDENTIAL_URL_AUTHORIZATION = 'URL_AUTHORIZATION',
-    RESIDENTIAL_URL_SALE = 'URL_SALE',
-    SMARTPHONE_URL_AUTHORIZATION = 'SMARTPHONE_URL_AUTHORIZATION',
-    SMARTPHONE_URL_SALE = 'SMARTPHONE_URL_SALE',
+    RESIDENTIAL_URL_PLANS = "URL_PLANS",
+    RESIDENTIAL_URL_ZIPCODE = "URL_ZIPCODE",
+    RESIDENTIAL_URL_AUTHORIZATION = "URL_AUTHORIZATION",
+    RESIDENTIAL_URL_SALE = "URL_SALE",
+    SMARTPHONE_URL_AUTHORIZATION = "SMARTPHONE_URL_AUTHORIZATION",
+    SMARTPHONE_URL_SALE = "SMARTPHONE_URL_SALE",
 }
 
 @injectable()
 export class RequestService {
-
     constructor(
         @inject(TYPES.ParameterStore)
         private parameterStore: ParameterStore,
         @inject(TYPES.AuthTokenService)
         private authTokenService: AuthTokenService
+    ) {}
+
+    METHODS = Methods;
+    ENDPOINTS = Endpoints;
+
+    async makeRequest(
+        url: Endpoints,
+        method: Methods,
+        body: object | null,
+        tenant: string,
+        queryString?: string
     ) {
-    }
+        log.debug(`Call to make a ${method} on ${url}`);
+        const apiUrl = await this.parameterStore.getSecretValue(url);
+        const token = await this.authTokenService.retrieveAuthorization(tenant);
 
-    METHODS = Methods
-    ENDPOINTS = Endpoints
+        log.debug(`Making ${method} to ${apiUrl}`);
 
-    async makeRequest(url:Endpoints, method:Methods, body: object | null, tenant: string, queryString?: string ) {
-
-        log.debug(`Call to make a ${method} on ${url}`)
-        const apiUrl = await this.parameterStore.getSecretValue(url)
-        const token = await this.authTokenService.retrieveAuthorization(tenant)
-
-        log.debug(`Making ${method} to ${apiUrl}`)
-
-        let headers
-        if(tenant === Tenants.SMARTPHONE){
+        let headers;
+        if (tenant === Tenants.SMARTPHONE) {
             headers = {
-                "Content-Type":"application/json",
-                "Authorization": token,
-                "apikey": await this.parameterStore.getSecretValue('SMARTPHONE_API_KEY')
-            }
+                "Content-Type": "application/json",
+                Authorization: token,
+                apikey: await this.parameterStore.getSecretValue(
+                    "SMARTPHONE_API_KEY"
+                ),
+            };
         }
-        if(tenant === Tenants.RESIDENTIAL) {
+        if (tenant === Tenants.RESIDENTIAL) {
             headers = {
-                "Content-Type":"application/json",
-                "Authorization": `Bearer ${token}`
-            }
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            };
+        }
+        if (tenant === Tenants.PET) {
+            headers = {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            };
         }
 
         let config = {
             method: method,
-            url: apiUrl + (queryString ? queryString : ''),
+            url: apiUrl + (queryString ? queryString : ""),
             headers,
-            data: JSON.stringify(body)
-        }
+            data: JSON.stringify(body),
+        };
 
-        let result = await axios(config)
+        let result = await axios(config);
 
-        log.debug("Request Successfully")
-        if(result.headers && result.headers['x-b3-traceid']){
-            log.debug(`Trace ID: ${result.headers['x-b3-traceid']}`)
+        log.debug("Request Successfully");
+        if (result.headers && result.headers["x-b3-traceid"]) {
+            log.debug(`Trace ID: ${result.headers["x-b3-traceid"]}`);
         }
 
         return result;
