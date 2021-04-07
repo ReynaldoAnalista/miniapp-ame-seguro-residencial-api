3edee229f18d183e058bd8a81f1e93d5a04cd243
Izabella Assad <izabellaassad@gmail.com>
2021-04-07 14:21:47 -0300

    routes

--- src/routes.ts
+++ src/routes.ts
@@ -227,7 +227,7 @@ export function RegisterRoutes(app: express.Express) {
             promiseHandler(controller, promise, response, next);
         });
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-    app.get('/ame-seguro-residencial/v1/pet/petList',
+    app.get('/ame-seguro-residencial/v1/pet/planList',
         function(request: any, response: any, next: any) {
             const args = {
             };
