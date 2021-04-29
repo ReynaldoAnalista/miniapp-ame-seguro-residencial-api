1ed8b6a572a6f0d3136ead70f16429034d4d95a4
Marcelo Madeira <marcelo.madeira@calindra.com.br>
2021-04-29 09:17:18 -0300

    Atualização. De teste e formato do objeto que recebe do pagamento

--- test/integration/pet/services/PetProposalService.test.ts
+++ test/integration/pet/services/PetProposalService.test.ts
@@ -1,11 +1,14 @@
 import path from "path";
 import util from "util";
 import fs from "fs";
-
+import jwt from "jsonwebtoken";
+import { v4 as uuidv4 } from 'uuid';
 import { initDependencies, iocContainer } from "../../../../src/inversify/inversify.config";
 import { PetProposalService } from "../../../../src/modules/petProposal/services/PetProposalService";
+import { ParameterStore } from "../../../../src/configs/ParameterStore";
 
 const readFile = util.promisify(fs.readFile)
+const sign = util.promisify(jwt.sign)
 
 initDependencies()
 jest.setTimeout(20000)
@@ -13,14 +16,25 @@ jest.setTimeout(20000)
 let petProposalService: PetProposalService
 describe("PetProposalService", () => {
 
+    let parameterStore: ParameterStore
+    let signedPayment: any
+
     beforeAll(async () => {
         petProposalService = iocContainer.get("PetProposalService")
+        parameterStore = iocContainer.get("ParameterStore")
+        const payment = await readFile(path.resolve(__dirname, "../../../fixtures/petNotification.json"), "utf-8")
+        console.log('Leu o arquivo de callback')
+        const secret = await parameterStore.getSecretValue("CALINDRA_JWT_SECRET")
+        console.log('Buscou o secret na AWS')
+        const paymentObject = JSON.parse(payment)
+        console.log('Realizou o parse do arquivo de callback')
+        paymentObject.id = uuidv4()
+        paymentObject.attributes.customPayload.customerId = uuidv4()
+        signedPayment = await sign(paymentObject, secret)
     })
     
     it("Envio da requisição de Pet", async() => {
-        const requestJson = await readFile(path.resolve(__dirname, "../../../fixtures/petNotification.json"), "utf-8")
-        const request = JSON.parse(requestJson)
-        const petService = await petProposalService.sendProposal(request)
+        const petService = await petProposalService.sendProposal(signedPayment)
         expect(petService?.status).toBe(true)
     }) 
 
