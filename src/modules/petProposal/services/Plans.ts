925ce084b69665f825d1b5e837fcf7523ee60972
Izabella Assad <izabellaassad@gmail.com>
2021-04-30 17:08:58 -0300

    alterando json plans

--- src/modules/petProposal/services/Plans.ts
+++ src/modules/petProposal/services/Plans.ts
@@ -7,32 +7,32 @@ export default [
         coverage: [
             {
                 description: "Transporte ao veterinário ",
-                value: "Até R$ 100,00",
+                value: "Até R$ 100,00 por utilização",
             },
             {
                 description: "Consulta emergencial",
-                value: "Até R$ 150,00",
+                value: "Até R$ 150,00 por utilização",
             },
             {
                 description: "Atendimento ambulatorial",
-                value: "Até R$ 200,00",
+                value: "Até R$ 200,00 por utilização",
             },
             {
                 description:
                     "Exames (básicos, complexos, imagem, cardiológicos e de alta complexidade)",
-                value: "Até R$ 500,00",
+                value: "Até R$ 500,00 por utilização",
             },
             {
                 description: "Cirurgias",
-                value: "Até R$ 750,00",
+                value: "Até R$ 750,00 por utilização",
             },
             {
                 description: "Internação",
-                value: "Até R$ 500,00",
+                value: "Até R$ 500,00 por utilização",
             },
             {
                 description: "Assistência funeral",
-                value: "Até R$ 400,00",
+                value: "Até R$ 400,00 por utilização",
             },
         ],
     },
@@ -44,40 +44,40 @@ export default [
         coverage: [
             {
                 description: "Aplicação de vacinas",
-                value: "Até R$ 150,00",
+                value: "Até R$ 150,00 por ano",
             },
             {
                 description: "Transporte ao veterinário",
-                value: "Até R$ 100,00",
+                value: "Até R$ 100,00 por utilização",
             },
             {
                 description: "Consulta emergencial",
-                value: "Até R$ 200,00",
+                value: "Até R$ 200,00 por utilização",
             },
             {
                 description: "Atendimento ambulatorial",
-                value: "Até R$ 250,00",
+                value: "Até R$ 250,00 por utilização",
             },
             {
                 description:
                     "Exames (básicos, complexos, imagem, cardiológicos e de alta complexidade)",
-                value: "Até R$ 750,00",
+                value: "Até R$ 750,00 por utilização",
             },
             {
                 description: "Cirurgias",
-                value: "Até R$ 1.400,00",
+                value: "Até R$ 1.400,00 por utilização",
             },
             {
                 description: "Iternação",
-                value: "Até R$ 1.000,00",
+                value: "Até R$ 1.000,00 por utilização",
             },
             {
                 description: "Acupuntura/ fisioterapia",
-                value: "Até R$ 500,00",
+                value: "Até R$ 500,00 por utilização",
             },
             {
                 description: "Assistência funeral",
-                value: "Até R$ 600,00",
+                value: "Até R$ 600,00 por utilização",
             },
         ],
     },
@@ -89,36 +89,36 @@ export default [
         coverage: [
             {
                 description: "Aplicação de vacinas",
-                value: "Até R$ 200,00",
+                value: "Até R$ 200,00 por ano",
             },
             {
                 description: "Transporte ao veterinário ",
-                value: "Até R$ 150,00",
+                value: "Até R$ 150,00 por utilização",
             },
             {
                 description: "Consulta emergencial",
-                value: "Até R$ 300,00",
+                value: "Até R$ 300,00 por utilização",
             },
             {
                 description: "Atendimento ambulatorial ",
-                value: "Até R$ 350,00",
+                value: "Até R$ 350,00 por utilização",
             },
             {
                 description:
                     "Exames (básicos, complexos, imagem, cardiológicos e de alta complexidade",
-                value: "Até R$ 1.000,00",
+                value: "Até R$ 1.000,00 por utilização",
             },
             {
                 description: "Cirurgias ",
-                value: "Até R$ 2.000,00",
+                value: "Até R$ 2.000,00 por utilização",
             },
             {
                 description: "Internação ",
-                value: "Até R$ 1.500,00",
+                value: "Até R$ 1.500,00 por utilização",
             },
             {
                 description: "Assistência funeral ",
-                value: "Até R$ 800,00",
+                value: "Até R$ 800,00 por utilização",
             },
         ],
     },
