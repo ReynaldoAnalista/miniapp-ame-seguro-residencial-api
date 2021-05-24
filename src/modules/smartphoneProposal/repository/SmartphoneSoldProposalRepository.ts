0556b7fd725eba2afb280bb75b87b789ab49ca31
Reynaldo <reynaldo.analista@gmail.com>
2021-05-24 17:30:06 -0300

    Ajuste do erro de validação

--- src/modules/smartphoneProposal/repository/SmartphoneSoldProposalRepository.ts
+++ src/modules/smartphoneProposal/repository/SmartphoneSoldProposalRepository.ts
@@ -40,9 +40,9 @@ export class SmartphoneSoldProposalRepository {
     async findcertificateNumber(certificateNumber: any) {
         let listProposal = await this.listSoldProposal()        
         return listProposal.map(element => {
-            if(certificateNumber.includes(element.control_data.control_data.key_contract_certificate_number)) {
+            if(certificateNumber.includes(element?.control_data?.control_data?.key_contract_certificate_number)) {
                 return {
-                    key_contract_certificate_number: element.control_data.control_data.key_contract_certificate_number,
+                    key_contract_certificate_number: element.control_data?.control_data?.key_contract_certificate_number,
                     customerId : element.customerId
                 } 
             }
