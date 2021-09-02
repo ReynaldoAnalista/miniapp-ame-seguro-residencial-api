840381298ff205ce4322bb09f912bd922f39e31c
Reynaldo Paixão de Souza <mestrerey@gmail.com>
2021-09-02 16:06:23 -0300

    fix: ajuste no valor do estorno

--- src/modules/maintenance/repository/MaintenanceRepository.ts
+++ src/modules/maintenance/repository/MaintenanceRepository.ts
@@ -36,4 +36,28 @@ export class MaintenanceRepository {
             return 0
         }
     }
+
+    async getCancelledOrder(customerId) {
+        log.debug(`Searching for Proposals in Table: ${TABLE}, customerId: ${customerId}`)
+        const params = {
+            TableName: TABLE,
+            KeyConditionExpression: "#customerId = :customerId",
+            ExpressionAttributeNames: {
+                "#customerId": "customerId",
+            },
+            ExpressionAttributeValues: {
+                ":customerId": customerId,
+            },
+        }
+        try {
+            const dynamoDocClient = await this.dynamoHolder.getDynamoDocClient()
+            const result = await dynamoDocClient.query(params).promise()
+            log.debug(`Have found ${result.Items?.length} items`)
+            return result.Items?.filter((x) => x.status == "CANCELED")
+        } catch (e) {
+            log.error(`Error on searching results from ${TABLE}`)
+            log.error(e)
+            return []
+        }
+    }
 }
