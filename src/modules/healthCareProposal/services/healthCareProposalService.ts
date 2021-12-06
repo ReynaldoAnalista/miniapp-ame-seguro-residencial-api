abf107814706b109a46497cf0da07a2e66e26b96
Reynaldo Paixão de Souza <mestrerey@gmail.com>
2021-12-06 16:29:51 -0300

    feat: cotacao do seguro mais saude

--- src/modules/healthCareProposal/services/healthCareProposalService.ts
+++ src/modules/healthCareProposal/services/healthCareProposalService.ts
@@ -125,9 +125,21 @@ export class HealthCareProposalService {
     async cotation(request: any) {
         const cotation = await readFile(path.resolve(__dirname, "../../../files/health_care_cotation.json"), "utf-8")
         const cotationObject = JSON.parse(cotation)
-        const finalCotation = cotationObject.filter(function (x) {
-            return request.age >= x.min && request.age <= x.max
-        })
+        const finalCotation = cotationObject
+            .filter((x) => request.age >= x.min && request.age <= x.max)
+            .map((x) => {
+                return {
+                    voce: (x.morte * request.range + x.ipa * request.range + x.diha + x.funeral + x.sorteio_liquido).toFixed(2),
+                    familia: (
+                        x.morte / 2 +
+                        x.morte * request.range +
+                        x.ipa * request.range +
+                        x.diha +
+                        x.funeral +
+                        x.sorteio_liquido
+                    ).toFixed(2),
+                }
+            })
         return finalCotation
     }
 
