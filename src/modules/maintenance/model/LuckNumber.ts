b2e20fa9355e31840d307601a80d54c34f514702
Reynaldo Paixão de Souza <mestrerey@gmail.com>
2022-02-08 12:02:22 -0300

    fix: inclusao do repositorio luck_number

---
+++ src/modules/maintenance/model/LuckNumber.ts
@@ -0,0 +1,10 @@
+export interface LuckNumber {
+    id: string
+    serial_number: string
+    luck_number: string
+    plan_number: string
+    order_number: string
+    register_number: string
+    keyRegister: string
+    used: string
+}
